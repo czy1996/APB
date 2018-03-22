0c92c2d0a05aba91fb12c0cfbeaea52ec760a106
helion <czy1996@users.noreply.github.com>
2018-03-22 10:25:51 +0800

    Ufe1, 2, 3, 4, 5

--- OilTemp.py
+++ OilTemp.py
@@ -1,81 +1,30 @@
 from sympy import *
 import json
 
+from Ufe import Ufe
+from _Symbols import _Symbols
 
-class OilTemperature:
+
+class OilTemperature(_Symbols):
     def __init__(self, params):
-        # 所有参数符号
-        self.init_symbols()
+        # 初始化符号
+        super().__init__()
+        # 结构参数
+        self.params = params
+
+        self._Ufe = Ufe(self)
+
+        # 替换计算参数 LR
+        self.TD = self.init_expr_TD()
+
+        self.Ufe = self._Ufe.expr()
+
+        # pprint(self.Ufe)
+
+        self.LR = self.init_expr_LR()
 
         # 地层温度表达式
         self.init_main_expr()
-
-        # 替换计算参数 LR
-        self.subs_symbol_LR()
-
-        self.subs_symbol_Ufe()
-        self.subs_symbol_TD()
-
-    def init_symbols(self):
-        # depth starting from bottom
-        self.Z = Symbol('Z')
-        # 井底处流体温度
-        self.T0 = Symbol('T0')
-        # 油层中部温度 摄氏度
-        self.Tr = Symbol('Tr')
-        # kg/m3
-        self.density_of_oil = Symbol('ρ')
-        # 定压比热容 J/(kg*K)
-        self.Cp = Symbol('Cp')
-        # 质量流量 kg/s
-        self.W = Symbol('W')
-        # 半稳态法得到的计算参数
-        self.LR = Symbol('LR')
-        # 地温梯度
-        self.m = Symbol('m')
-        # 重力加速度 m/s^2
-        self.g = Symbol('g')
-        # 包含了动能项和焦耳-汤姆逊系数（J--T）的计算参数
-        self.fi = Symbol('∅')
-
-        # 油管内径
-        self.rti = Symbol('rti')
-
-        # 导热系数
-        self.Ke = Symbol('Ke')
-
-        # 该微元段传热系数
-        self.Ufe = Symbol('Ufe')
-
-        # 无因此地层温度
-        self.TD = Symbol('TD')
-        # 油管内壁对流换热系数
-        self.h = Symbol('h')
-
-        # K 导热系数，下标 t-油管，a-环空，c-套管 cem-水泥环
-
-        self.rto = Symbol('rto')
-
-        self.Kt = Symbol('Kt')
-
-        self.Ka = Symbol('Ka')
-        self.rc1i = Symbol('rc1i')
-        self.Kc = Symbol('Kc')
-        self.rc1o = Symbol('rc1o')
-        self.rc2i = Symbol('rc2i')
-        self.rc2o = Symbol('rc2o')
-        self.rc3i = Symbol('rc3i')
-        self.rc3o = Symbol('rc3o')
-        self.Kcem = Symbol('Kcem')
-        self.rcem = Symbol('rcem')
-
-        self.t = Symbol('t')
-
-        # 地层热扩散系数
-        self.ae = Symbol('ae')
-
-        # 井筒尺寸
-        self.rw = Symbol('rw')
 
     def init_main_expr(self):
         self.power_of_exp = -self.LR / (self.W * self.Cp) * self.Z
@@ -84,51 +33,33 @@ class OilTemperature:
                     + (1 - exp(self.power_of_exp)) * (self.m - self.g / self.Cp + self.fi) * self.W * self.Cp / self.LR \
                     + self.Tr - self.m * self.Z
 
-    def subs_symbol_LR(self):
+    def init_expr_LR(self):
         # 替换计算参数 LR
 
         # rti?
         LR = 2 * pi * self.rti * self.Ufe * self.Ke / (self.Ke + self.rti * self.Ufe * self.TD)
 
-        self.expr = self.expr.subs(self.LR, LR)
+        return LR
 
-    def subs_symbol_Ufe(self):
-        Ufe = 1 / \
-              (1 / self.h
-               + self.rti / self.Kt * ln(self.rto / self.rti)
-               + self.rti / self.Ka * ln(self.rc1i / self.rto)
-               + self.rti / self.Kc * ln(self.rc1o / self.rc1i)
-               + self.rti / self.Ka * ln(self.rc2i / self.rc1o)
-               + self.rti / self.Kc * ln(self.rc2o / self.rc2i)
-               + self.rti / self.Ka * ln(self.rc3i / self.rc2o)
-               + self.rti / self.Kc * ln(self.rc3o / self.rc3i)
-               + self.rti / self.Kcem * ln(self.rcem / self.rc3o)
-               )
-        self.expr = self.expr.subs(self.Ufe, Ufe)
-
-    def subs_symbol_TD(self):
+    def init_expr_TD(self):
         td = self.t * self.ae / self.rw ** 2
 
         TD = ln(exp(-0.2 * td) +
                 (1.5 - 0.3719 * exp(-td)) * sqrt(td))
 
-        self.expr = self.expr.subs(self.TD, TD)
+        return TD
 
-    def Ufe_by_depth(self, depth):
-        """
 
-        :param depth:
-        :return: 该深度处的 Ufe
-        """
-        pass
 
 
 if __name__ == '__main__':
     init_printing(use_unicode=True)
 
     with open('data.json', 'r') as f:
-        params = f.read()
+        params = json.load(f)
 
     oil_temp = OilTemperature(params)
 
-    pprint(oil_temp.expr, use_unicode=True)
+    # pprint(params)
+
+    print(latex(oil_temp.expr))
---
+++ Ufe.py
@@ -0,0 +1,145 @@
+from sympy import *
+
+from _Symbols import _Symbols
+
+
+class Ufe(_Symbols):
+    def __init__(self, temp):
+        super().__init__()
+        self.temp = temp
+        self.params = temp.params
+        self.depth = self.params['well']['casing1']['depth'] - self.temp.Z
+
+    def expr(self):
+        depth_of_casing2 = self.params['well']['casing2']['depth']
+        Ufe_1 = (
+            self.Ufe_1(),
+            self.depth > depth_of_casing2,
+        )
+
+        toc_of_casing1 = self.params['well']['casing1']['toc']
+        Ufe_2 = (
+            self.Ufe_2(),
+            self.depth > toc_of_casing1,
+        )
+
+        depth_of_casing3 = self.params['well']['casing3']['depth']
+        Ufe_3 = (
+            self.Ufe_3(),
+            self.depth > depth_of_casing3,
+        )
+
+        toc_of_casing2 = self.params['well']['casing2']['toc']
+        Ufe_4 = (
+            self.Ufe_4(),
+            self.depth > toc_of_casing2,
+        )
+
+        Ufe_5 = (
+            self.Ufe_5(),
+            self.depth > 0,
+        )
+
+        expr = Piecewise(
+            Ufe_1,
+            Ufe_2,
+            Ufe_3,
+            Ufe_4,
+            Ufe_5,
+        )
+
+        return expr
+
+    def Ufe_1(self):
+        """
+        从井底到第二层套管鞋
+        :return:
+        """
+        Ufe = 1 / \
+              (1 / self.h
+               + self.rti / self.Kt * ln(self.rto / self.rti)
+               + self.rti / self.Ka * ln(self.rc1i / self.rto)
+               + self.rti / self.Kc * ln(self.rc1o / self.rc1i)
+               + self.rti / self.Kcem * ln(self.rc2i / self.rc1o)
+               )
+
+        return Ufe
+
+    def Ufe_2(self):
+        """
+        从第二层套管鞋到第一层 toc (油层套管 toc)
+        :return:
+        """
+        Ufe = 1 / \
+              (1 / self.h
+               + self.rti / self.Kt * ln(self.rto / self.rti)
+               + self.rti / self.Ka * ln(self.rc1i / self.rto)
+               + self.rti / self.Kc * ln(self.rc1o / self.rc1i)
+               + self.rti / self.Kcem * ln(self.rc2i / self.rc1o)
+               + self.rti / self.Kc * ln(self.rc2o / self.rc2i)
+               + self.rti / self.Kcem * ln(self.rc3i / self.rc2o)
+               )
+
+        return Ufe
+
+    def Ufe_3(self):
+        """
+        从第一层 toc (油层套管 toc) 到表层套管鞋
+        :return:
+        """
+        Ufe = 1 / \
+              (1 / self.h
+               + self.rti / self.Kt * ln(self.rto / self.rti)
+               + self.rti / self.Ka * ln(self.rc1i / self.rto)
+               + self.rti / self.Kc * ln(self.rc1o / self.rc1i)
+               + self.rti / self.Ka * ln(self.rc2i / self.rc1o)
+               + self.rti / self.Kc * ln(self.rc2o / self.rc2i)
+               + self.rti / self.Kcem * ln(self.rc3i / self.rc2o)
+               )
+
+        return Ufe
+
+    def Ufe_4(self):
+        Ufe = 1 / \
+              (1 / self.h
+               + self.rti / self.Kt * ln(self.rto / self.rti)
+               + self.rti / self.Ka * ln(self.rc1i / self.rto)
+               + self.rti / self.Kc * ln(self.rc1o / self.rc1i)
+               + self.rti / self.Ka * ln(self.rc2i / self.rc1o)
+               + self.rti / self.Kc * ln(self.rc2o / self.rc2i)
+               + self.rti / self.Kcem * ln(self.rc3i / self.rc2o)
+               + self.rti / self.Kc * ln(self.rc3o / self.rc3i)
+               + self.rti / self.Kcem * ln(self.rcem / self.rc3o)
+               )
+
+        return Ufe
+
+    def Ufe_5(self):
+        Ufe = 1 / \
+              (1 / self.h
+               + self.rti / self.Kt * ln(self.rto / self.rti)
+               + self.rti / self.Ka * ln(self.rc1i / self.rto)
+               + self.rti / self.Kc * ln(self.rc1o / self.rc1i)
+               + self.rti / self.Ka * ln(self.rc2i / self.rc1o)
+               + self.rti / self.Kc * ln(self.rc2o / self.rc2i)
+               + self.rti / self.Ka * ln(self.rc3i / self.rc2o)
+               + self.rti / self.Kc * ln(self.rc3o / self.rc3i)
+               + self.rti / self.Kcem * ln(self.rcem / self.rc3o)
+               )
+
+        return Ufe
+
+    def __Ufe_full(self):
+        Ufe = 1 / \
+              (1 / self.h
+               + self.rti / self.Kt * ln(self.rto / self.rti)
+               + self.rti / self.Ka * ln(self.rc1i / self.rto)
+               + self.rti / self.Kc * ln(self.rc1o / self.rc1i)
+               + self.rti / self.Ka * ln(self.rc2i / self.rc1o)
+               + self.rti / self.Kc * ln(self.rc2o / self.rc2i)
+               + self.rti / self.Ka * ln(self.rc3i / self.rc2o)
+               + self.rti / self.Kc * ln(self.rc3o / self.rc3i)
+               + self.rti / self.Kcem * ln(self.rcem / self.rc3o)
+               )
+
+        return Ufe
