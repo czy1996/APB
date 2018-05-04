590152499cc9dc5149dec0d47cc9d2f5e525bc05
helion <czy1996@users.noreply.github.com>
2018-05-04 13:30:10 +0800

    data.json 油管直径

---
+++ qt_demo.py
@@ -0,0 +1,18 @@
+import sys
+
+from mainWindow import Ui_MainWindow
+from PyQt5 import QtWidgets
+
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
